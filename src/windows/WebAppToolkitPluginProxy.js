9f43d9c922d9fbe4cb0fc5c4323648dfd9a376f4
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-03-30 20:02:24 -0300

    [Windows] Consume the notification with the loaded manifest from the hosted app plugin

--- src/windows/WebAppToolkitPluginProxy.js
+++ src/windows/WebAppToolkitPluginProxy.js
@@ -1,7 +1,21 @@
+var manifest;
+
 module.exports = {
   initialize: function (successCallback, errorCallback, args) {
     //TODO
   }
 }; // exports
 
+function manifestLoaded(evt) {
+  manifest = evt.manifest;
+}
+
 cordova.commandProxy.add("WebAppToolkit", module.exports);
+
+document.addEventListener('manifestLoaded', manifestLoaded, false);
+
+if (!manifest) {
+  cordova.commandProxy.get('HostedWebApp', 'getManifest')(function successCallback(manifestData) {
+    manifest = manifestData;
+  });
+}
