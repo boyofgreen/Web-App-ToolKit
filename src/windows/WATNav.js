11670b6853bc7f85af48d5219813785b13cc8ed1
alejandro.jezierski <alejandro.jezierski@southworks.net>
2015-04-07 22:30:02 +0200

    [wat-plugin][windows] Basic back button for windows, with basic toggle and hardcoded styles

--- src/windows/WATNav.js
+++ src/windows/WATNav.js
@@ -1,8 +1,8 @@
 
 "use strict";
 
-var configureBackButton, navigateBack;
-var WAT, navConfig;
+var createBackButton, configureBackButton, navigateBack;
+var WAT, navConfig, webViewNavStart, webViewNavComplete;
 
 // Public API
 var self = {
@@ -12,22 +12,43 @@ var self = {
       navConfig = WAT.manifest.wat_navigation || {};
 
       configureBackButton(WATref);
+
+      WAT.components.webView.addEventListener("MSWebViewNavigationStarting", webViewNavStart);
+      WAT.components.webView.addEventListener("MSWebViewNavigationCompleted", webViewNavComplete);
+
     }
+  },
+
+  toggleBackButton: function (makeVisible) {
+      if (makeVisible) {
+          WAT.components.backButton.style.display = "block";
+      }
+      else
+      {
+          WAT.components.backButton.style.display = "none";
+      }
   }
 };
 
 // Private methods
 
 configureBackButton = function () {
-  WinJS.UI.processAll().then(function () {
-    // Back button
-    WinJS.Application.onbackclick = navigateBack;
-  });
+    WinJS.UI.processAll().then(function () {
+        // create back button
+        createBackButton();
 
-  if (WAT.components.backButton) {
-    // handle back button clicks
-    WAT.components.backButton.addEventListener("click", navigateBack);
-  }
+        // Back button
+        WinJS.Application.onbackclick = navigateBack;
+
+        if (WAT.components.backButton) {
+            // handle back button clicks
+            WAT.components.backButton.addEventListener("click", navigateBack);
+
+            if (!WAT.components.webView.canGoBack) {
+                self.toggleBackButton(false);
+            }
+        }
+    });
 };
 
 navigateBack = function (e) {
@@ -46,5 +67,37 @@ navigateBack = function (e) {
   return true;
 }
 
+createBackButton = function () {
+    var wrapperDiv = document.createElement("div");
+
+    wrapperDiv.id = "backbutton-wrapper";
+    wrapperDiv.style.zIndex = WAT.components.webView.style.zIndex + 100;
+    wrapperDiv.style.position = "absolute";
+    wrapperDiv.style.top = "0";
+    wrapperDiv.style.left = "0";
+    wrapperDiv.style.padding = "50px";
+    wrapperDiv.style.width = "150px";
+    wrapperDiv.style.height = "50px";
+
+    var btn = document.createElement("btn");
+    btn.classList.add("win-backbutton");
+    btn.style.color = "black";
+    btn.style.borderColor = "black";
+
+    WAT.components.backButton = wrapperDiv;
+
+    wrapperDiv.appendChild(btn);
+    document.body.appendChild(wrapperDiv);
+}
+
+webViewNavStart = function (e) {
+    self.toggleBackButton(false);
+}
+
+webViewNavComplete = function (e) {
+    if (WAT.components.webView.canGoBack) {
+        self.toggleBackButton(true);
+    }
+}
 
 module.exports = self; // exports
