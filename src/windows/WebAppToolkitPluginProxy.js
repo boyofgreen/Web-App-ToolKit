1d1a6e0fee1daa7074087638aa0bda0a26bce584
alejandro.jezierski <alejandro.jezierski@southworks.net>
2015-05-15 11:40:33 -0300

    [windows] Wat_settings support for Windows

--- src/windows/WebAppToolkitPluginProxy.js
+++ src/windows/WebAppToolkitPluginProxy.js
@@ -127,6 +127,7 @@ function initialize() {
       require('com.microsoft.webapptoolkit.WATCustomScript').init(WAT);
       require('com.microsoft.webapptoolkit.WATNavBar').init(WAT);
       require('com.microsoft.webapptoolkit.WATHeader').init(WAT);
+      require('com.microsoft.webapptoolkit.WATSettings').init(WAT);
     }
   }
 }
